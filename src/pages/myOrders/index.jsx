7876e87b824e093986679b3dc18f84323258aa4b
Kellbis <holasoykellbis@gmail.com>
2025-01-05 22:47:28 -0400

    sm advance in MyOrders

--- src/pages/myOrders/index.jsx
+++ src/pages/myOrders/index.jsx
@@ -1,12 +1,52 @@
 import React, { useContext } from 'react';
 import { Layout } from '../../components/layout';
 import { CartContext } from '../../context/CartContext';
+import { AuthContext } from '../../context/authContext';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDollar } from '@fortawesome/free-solid-svg-icons';
 
 function MyOrders() {
 	const { orders } = useContext(CartContext);
-	if (orders.length > 0) {
-		return <Layout>MyOrder</Layout>;
+	const { userId } = useContext(AuthContext);
+
+	const userOrders = orders.filter((order) => order.buyerId === userId);
+	// console.log(userOrders);
+
+	if (userOrders) {
+		return (
+			<Layout>
+				<div className="flex flex-col w-[70vw] p-6 gap-6">
+					{userOrders.map((order, index) => (
+						<div className="flex justify-between items-center overflow-hidden bg-gray-200 p-6 rounded-lg" key={index}>
+							<div className="p-4">
+								<h2>{order.date}</h2>
+							</div>
+
+							<div className="flex justify-between w-full p-2">
+								<div className="flex-1 flex overflow-hidden flex-wrap justify-center">
+									{order.products.slice(0, 3).map((product, productIndex) => (
+										<div key={productIndex} className="bg-white rounded-lg w-20 md:w-32 h-24 md:h-32 p-2 m-2">
+											<img src={product.image} className="w-full h-full object-contain object-center" />
+										</div>
+									))}
+									{order.products.length > 3 && (
+										<div className="bg-gray-100 rounded-lg w-20 md:w-32 h-24 md:h-32 p-2 m-2 flex justify-center items-center">
+											<p>+{order.products.length - 3} more</p>
+										</div>
+									)}
+								</div>
+							</div>
+
+							<div className="flex items-center justify-between p-4">
+								<p>{order.totalPrice}</p>
+								<FontAwesomeIcon className="pl-1 color text-green-500" icon={faDollar} />
+							</div>
+						</div>
+					))}
+				</div>
+			</Layout>
+		);
 	} else {
 		return (
 			<Layout>
